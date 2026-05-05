668d8c5c5807af42ab2cd775f493554106e7fa3b
Karen-Giovanna-Southier <karengiovannasouthier1@gmail.com>
2026-05-05 20:16:51 -0300

    feat: projeto inicial

---
+++ assets/flags.js
@@ -0,0 +1,19 @@
+const flags = {
+  MEX: require("./jogos/mexico.png"),
+  RSA: require("./jogos/south africa.png"),
+  KOR: require("./jogos/south korea.png"),
+  CZE: require("./jogos/czech republic.png"),
+  BRA: require("./jogos/brazil.png"),
+  ARG: require("./jogos/argentina.png"),
+  FRA: require("./jogos/france.png"),
+  ALE: require("./jogos/germany.png"),
+  ENG: require("./jogos/england.png"),
+  ESP: require("./jogos/spain.png"),
+  POR: require("./jogos/portugal.png"),
+  ITA: require("./jogos/italy.png"),
+  USA: require("./jogos/usa.png"),
+  JPN: require("./jogos/japan.png"),
+  AUS: require("./jogos/australia.png"),
+  MAR: require("./jogos/morocco.png")
+};
+export default flags;
