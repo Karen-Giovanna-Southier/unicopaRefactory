7205f22fb03c702e2279f61e48962aecb700c2f5
Karen-Giovanna-Southier <karengiovannasouthier1@gmail.com>
2026-06-06 21:19:01 -0300

    feat: RF17: tela para ver todos os palpites

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 import { registerRootComponent } from 'expo';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import App from './App';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import PalpitesScreen from './screens/PalpitesScreen';
+import MeusPalpitesScreen from './screens/MeusPalpitesScreen';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+const Stack = createNativeStackNavigator();
+
+function Root() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Home" component={App} />
+        <Stack.Screen name="Palpites" component={PalpitesScreen} />
+        <Stack.Screen name="MeusPalpites" component={MeusPalpitesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+registerRootComponent(Root);
